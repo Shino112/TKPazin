2d8c3ff931bf405f3ca666b7ac578beb0d806910
Shino112 <42404913+Shino112@users.noreply.github.com>
2020-06-01 16:06:38 +0200

    Update script.blade.php

--- resources/views/backend/novosti/script.blade.php
+++ resources/views/backend/novosti/script.blade.php
@@ -1,6 +1,22 @@
 @section('script')
     <script type="text/javascript">
 
-        var simplemde1 = new SimpleMDE({ element: $("#tekst")[0] });
+        var simplemde1 = new SimpleMDE({ element: $("#tekst")[0] 
+            toolbar: [{
+                name: "bold",
+                action: SimpleMDE.toggleBold,
+                className: "fa fa-bold",
+                title: "Bold",
+            },
+            {
+                name: "custom",
+                action: SimpleMDE.drawTable,
+                className: "fa fa-table",
+                title: "Insert Table",
+            },
+            "|", // Separator
+            ...
+        ],
+        });
     </script>
 @endsection

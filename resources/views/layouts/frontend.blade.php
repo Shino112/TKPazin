2a119931e82314627b80b31a68f05e263042e410
Shino112 <luka.otocan@student.uniri.hr>
2020-06-11 16:28:09 +0200

    added in head for Google AdSense

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     @yield('css')
-
+    <script data-ad-client="ca-pub-7404475039697434" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg fixed-top navbar-light" style="background-color: #000000;">
